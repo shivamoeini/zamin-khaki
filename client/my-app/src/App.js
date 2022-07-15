0f7f9793e9d1e49dd7494593e06d055d87e0366c
shiva moeini <shivamoeini2000@gmail.cpm>
2022-07-15 16:48:17 +0430

    fullname

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -14,13 +14,5 @@
 // } from "@material-ui/core";
 
 // const App = () => {
-//   return <>
-//       <CssBaseline/>
-//       <AppBar position="relative">
-//         <Toolbar>
-//          <Typography variant="h6">phto album</Typography>
-//         </Toolbar>
-//       </AppBar>
-//          </>;
-// };
+
 // export default App;
--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-
+function formatName(user){
+return user.firstName+' '+user.lastName;
+}
+const user={
+  firstName:"shiva",
+  lastName:"moeini"
+};
+const element=<h1>Welcome , {formatName(user)} ReactJS</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<h1>hello world</h1>
+<h1>{element}</h1>
 );
