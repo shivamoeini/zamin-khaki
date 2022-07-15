35963d17438643da0af3b81a7c61d0f497de4771
shiva moeini <shivamoeini2000@gmail.cpm>
2022-07-15 15:21:31 +0430

    cmit2

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,35 +1,26 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import itemData from './itemData';
- 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  imageList: {
-    width: 500,
-    height: 450,
-  },
-}));
+// import React from "react";
 
-export default function BasicImageList() {
-  const classes = useStyles();
+// import {
+//   Typography,
+//   AppBar,
+//   Card,
+//   CardActions,
+//   CardContent,
+//   CardMedia,
+//   CssBaseline,
+//   Grid,
+//   Toolbar,
+//   Container,
+// } from "@material-ui/core";
 
-  return (
-    <div className={classes.root}>
-      <ImageList rowHeight={160} className={classes.imageList} cols={3}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img} cols={item.cols || 1}>
-            <img src={item.img} alt={item.title} />
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </div>
-  );
-}
+// const App = () => {
+//   return <>
+//       <CssBaseline/>
+//       <AppBar position="relative">
+//         <Toolbar>
+//          <Typography variant="h6">phto album</Typography>
+//         </Toolbar>
+//       </AppBar>
+//          </>;
+// };
+// export default App;
