755fc599de20378831803ce91fa445a9c16f2d06
shiva moeini <shivamoeini2000@gmail.cpm>
2022-07-16 13:37:23 +0430

    App-componnet-welcome

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
 function Welcome(props){
   return <h1>Hello,{props.name}</h1>
 }
+
+function App(){
+  return(
+    <div>
+      <Welcome name="sara"/>
+      <Welcome name="shiva"/>
+      <Welcome name="shila"/>
+    </div>
+  );
+}
+// 1) We call root.render() with the <Welcome name="Sara" /> element.
+// 2) React calls the Welcome component with {name: 'Sara'} as the props.
+// 3) Our Welcome component returns a <h1>Hello, Sara</h1> element as the result.
+// 4) React DOM efficiently updates the DOM to match <h1>Hello, Sara</h1>.
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const element=<Welcome name="shiva"/>
 root.render(
- element
+ <App/>
 );
