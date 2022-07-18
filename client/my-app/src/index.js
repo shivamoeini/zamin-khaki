b33461cee4b9a2ba73b309570b6b3209a0663eaa
shiva moeini <shivamoeini2000@gmail.cpm>
2022-07-19 00:27:03 +0430

    state

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -99,24 +99,49 @@ import ReactDOM from "react-dom/client";
 //     )
 //   }
 // }
+// class Clock extends React.Component{
+//   constructor(props){
+//     super(props);
+//       this.state={date:new Date()};
+//     }
+//     render(){
+//       return(
+//         <div>
+//         <h1>Hello,World</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//         </div>
+//       )
+//     }
+
+//   }
 class Clock extends React.Component{
-  constructor(props){
-    super(props);
-      this.state={date:new Date()};
-    }
-    render(){
-      return(
-        <div>
-        <h1>Hello,World</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-      )
-    }
-
+  constructor(){
+    super();
+    this.state={date:new Date()};
   }
-
- 
-
+  componentDidMount(){
+    this.timerID=setInterval(
+      ()=>this.tick(),1000
+    );
+  }
+  componentWillUnmount(){
+    clearInterval(this.timerID)
+  }
+  tick(){
+   this.setState({
+     date:new Date()
+   });
+  }
+  
+ render(){
+  return(
+    <div>
+      <h1>Hello,World!</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+    </div>
+  )
+ }
+}
   const root = ReactDOM.createRoot(document.getElementById('root'));
 
   root.render(<Clock />)
